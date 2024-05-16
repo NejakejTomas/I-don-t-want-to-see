21c155335c012fdaeda370acc3802be240ec97e1
Tomáš Kiec <tomas@kiec.cz>
2024-05-16 03:06:14 +0200

    Use per-app locales

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,11 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+
+    androidResources {
+        @Suppress("UnstableApiUsage")
+        generateLocaleConfig = true
+    }
 }
 
 dependencies {
